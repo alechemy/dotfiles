f4c76401c77c238e359137da3dbb3904f5f4b99e
Alec Custer <alcuster@icloud.com>
2020-11-25 16:11:53 -0500

    tabs to spaces

--- hammerspoon/install.sh
+++ hammerspoon/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if command -v mdfind -name 'kMDItemFSName=="Hammerspoon.app"' >/dev/null; then
-	HAMMERSPOON_HOME="$HOME/.hammerspoon"
-	mkdir -p "$HAMMERSPOON_HOME/apps"
+    HAMMERSPOON_HOME="$HOME/.hammerspoon"
+    mkdir -p "$HAMMERSPOON_HOME/apps"
 
     ln -sf "$DOTFILES/hammerspoon/init.lua" "$HAMMERSPOON_HOME/init.lua"
     ln -sf "$DOTFILES/hammerspoon/apps/init.lua" "$HAMMERSPOON_HOME/apps/init.lua"
