f233f9d801ddf5db47801dbffe17d47ccc109d69
Alec Custer <alcuster@icloud.com>
2021-01-03 17:08:58 -0500

    more tweaks to espanso config

--- espanso/install.sh
+++ espanso/install.sh
@@ -3,4 +3,5 @@ if command -v espanso >/dev/null; then
 	ESPANSO_HOME="$HOME/Library/Preferences/espanso"
 
 	ln -sf "$DOTFILES/espanso/default.yml" "$ESPANSO_HOME/default.yml"
+	ln -sf "$DOTFILES/espanso/private.yml" "$ESPANSO_HOME/user/private.yml"
 fi
