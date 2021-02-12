dccb1ebfb232f00df58c31fc9eddaa782c4f9b71
Alec Custer <alcuster@icloud.com>
2021-02-12 15:44:29 -0500

    better zsh keybindings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,8 +6,7 @@ fpath=($DOTFILES/functions $fpath)
 fpath=($DOTFILES/ffs $fpath)
 
 autoload -U $fpath[1]/*(.:t)
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
+autoload -U history-search-end
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
@@ -31,16 +30,13 @@ setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-
+# emacs mode
 bindkey -e
 
-# fuzzy find: start to type
-bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
-bindkey "$terminfo[kcud1]" down-line-or-beginning-search
-bindkey "$terminfo[cuu1]" up-line-or-beginning-search
-bindkey "$terminfo[cud1]" down-line-or-beginning-search
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
 
 # search history with fzf if installed, default otherwise
 if test -d /usr/local/opt/fzf/shell; then
